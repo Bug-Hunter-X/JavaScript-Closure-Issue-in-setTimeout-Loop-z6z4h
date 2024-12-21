5b2052bbe20c836235a5bc70a4e54ba21061a441
BugHunter-X <askzenai@gmail.com>
2024-12-22 00:42:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,22 @@
+function myFunction() {
+  for (let i = 0; i < 10; i++) {
+    (function(i) {
+      setTimeout(function() {
+        console.log(i);
+      }, 1000);
+    })(i);
+  }
+}
+
+myFunction();
+
+//Alternative using let:
+function myFunction2() {
+  for (let i = 0; i < 10; i++) {
+    setTimeout(function() {
+      console.log(i);
+    }, 1000);
+  }
+}
+
+myFunction2();
